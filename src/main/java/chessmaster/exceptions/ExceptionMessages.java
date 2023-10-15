75a416c5acfbe1ad67323668dac625a535b0be8a
TongZhengHong <zhtong315@gmail.com>
2023-10-15 23:47:41 +0800

    Create basic exceptions

---
+++ src/main/java/chessmaster/exceptions/ExceptionMessages.java
@@ -0,0 +1,6 @@
+package chessmaster.exceptions;
+
+public class ExceptionMessages {
+    public static final String MESSAGE_PARSE_CHESS_PIECE_EXCEPTION = "Unable to parse chess piece!";
+    public static final String MESSAGE_PARSE_COORDINATE_EXCEPTION = "Unable to parse coordinate!";
+}
