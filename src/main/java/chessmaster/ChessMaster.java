c1cad31c65a0396670d913dee75b41918f2f7bdb
TongZhengHong <zhtong315@gmail.com>
2023-10-16 00:59:38 +0800

    Fix IO redirection test

--- src/main/java/chessmaster/ChessMaster.java
+++ src/main/java/chessmaster/ChessMaster.java
@@ -9,31 +9,32 @@ import chessmaster.ui.TextUI;
 public class ChessMaster {
     public static void main(String[] args) {
 
-        String logo = "░█████╗░██╗░░██╗███████╗░██████╗░██████╗   ███╗░░░███╗░█████╗░░██████╗████████╗███████╗██████╗░"
-                + System.lineSeparator() +
-                "██╔══██╗██║░░██║██╔════╝██╔════╝██╔════╝   ████╗░████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗"
-                + System.lineSeparator() +
-                "██║░░╚═╝███████║█████╗░░╚█████╗░╚█████╗░   ██╔████╔██║███████║╚█████╗░░░░██║░░░█████╗░░██████╔╝"
-                + System.lineSeparator() +
-                "██║░░██╗██╔══██║██╔══╝░░░╚═══██╗░╚═══██╗   ██║╚██╔╝██║██╔══██║░╚═══██╗░░░██║░░░██╔══╝░░██╔══██╗"
-                + System.lineSeparator() +
-                "╚█████╔╝██║░░██║███████╗██████╔╝██████╔╝   ██║░╚═╝░██║██║░░██║██████╔╝░░░██║░░░███████╗██║░░██║"
-                + System.lineSeparator() +
-                "░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░   ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝"
-                + System.lineSeparator();
+    //     String logo = "░█████╗░██╗░░██╗███████╗░██████╗░██████╗   ███╗░░░███╗░█████╗░░██████╗████████╗███████╗██████╗░"
+    //             + System.lineSeparator() +
+    //             "██╔══██╗██║░░██║██╔════╝██╔════╝██╔════╝   ████╗░████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗"
+    //             + System.lineSeparator() +
+    //             "██║░░╚═╝███████║█████╗░░╚█████╗░╚█████╗░   ██╔████╔██║███████║╚█████╗░░░░██║░░░█████╗░░██████╔╝"
+    //             + System.lineSeparator() +
+    //             "██║░░██╗██╔══██║██╔══╝░░░╚═══██╗░╚═══██╗   ██║╚██╔╝██║██╔══██║░╚═══██╗░░░██║░░░██╔══╝░░██╔══██╗"
+    //             + System.lineSeparator() +
+    //             "╚█████╔╝██║░░██║███████╗██████╔╝██████╔╝   ██║░╚═╝░██║██║░░██║██████╔╝░░░██║░░░███████╗██║░░██║"
+    //             + System.lineSeparator() +
+    //             "░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░   ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝"
+    //             + System.lineSeparator();
 
-        System.out.println(logo);
+    //     System.out.println(logo);
 
         TextUI ui = new TextUI();
         ChessBoard board = new ChessBoard();
+        board.showChessBoard(ui);
 
-        while (true) {
-            board.showChessBoard(ui);
-            // String move = ui.getUserCommand();
-            // TODO: Check if move is valid
-            // TODO: Update chessboard (new position with chesspiece, old position empty)
-            // TODO: Update ChessPiece (position)
-            // TODO: Store updated board in text file
-        }
+        // while (true) {
+        //     board.showChessBoard(ui);
+        //     String move = ui.getUserCommand();
+        //     TODO: Check if move is valid
+        //     TODO: Update chessboard (new position with chesspiece, old position empty)
+        //     TODO: Update ChessPiece (position)
+        //     TODO: Store updated board in text file
+        // }
     }
 }
