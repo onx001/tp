aad8c9191d3d81e12fc1a3d26a0e90353127740c
ken_ruster <ken.ruster@gmail.com>
2023-10-16 21:40:56 +0800

    Update documentation

--- src/main/java/chessmaster/parser/Parser.java
+++ src/main/java/chessmaster/parser/Parser.java
@@ -12,11 +12,13 @@ import chessmaster.pieces.Pawn;
 import chessmaster.pieces.ChessPiece;
 import chessmaster.game.Move;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Parser {
+    /**
+     * Parses the user's typed input into the UI and executes actions accordingly.
+     *
+     * @param in    Command entered by the user.
+     * @param board Chessboard the user is currently playing on.
+     */
     public void parseCommand(String in, ChessBoard board) {
         String commandWord = in.split(" ")[0].toLowerCase();
 
@@ -35,9 +37,11 @@ public class Parser {
      * Parses an input string and returns the move indicated by the string.
      * Used to read user inputs during the chess game.
      *
-     * @param in
-     * @param board
-     * @return
+     * @param in    String containing the user's intended move.
+     * @param board The chessboard the user is currently playing on.
+     * @return Move class containing information about the move to be made.
+     *
+     * @throws ParseCoordinateException If the string entered does not match a coordinate.
      */
     public Move parseMove(String in, ChessBoard board) throws ParseCoordinateException {
         Coordinate[] moveArray = new Coordinate[2];
