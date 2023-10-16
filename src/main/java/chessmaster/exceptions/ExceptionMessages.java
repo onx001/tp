5c3dcaac28b2874d45e7b11eca8bc53914f195dd
ken_ruster <ken.ruster@gmail.com>
2023-10-16 21:33:13 +0800

    Update message for InvalidMoveException

--- src/main/java/chessmaster/exceptions/ExceptionMessages.java
+++ src/main/java/chessmaster/exceptions/ExceptionMessages.java
@@ -3,4 +3,5 @@ package chessmaster.exceptions;
 public class ExceptionMessages {
     public static final String MESSAGE_PARSE_CHESS_PIECE_EXCEPTION = "Unable to parse chess piece!";
     public static final String MESSAGE_PARSE_COORDINATE_EXCEPTION = "Unable to parse coordinate!";
+    public static final String MESSAGE_INVALID_MOVE_EXCEPTION = "Oops, that move isn't valid!";
 }
--- src/main/java/chessmaster/exceptions/InvalidMoveException.java
+++ src/main/java/chessmaster/exceptions/InvalidMoveException.java
@@ -2,7 +2,7 @@ package chessmaster.exceptions;
 
 public class InvalidMoveException extends Exception{
     public InvalidMoveException() {
-        super(ExceptionMessages.MESSAGE_PARSE_CHESS_PIECE_EXCEPTION);
+        super(ExceptionMessages.MESSAGE_INVALID_MOVE_EXCEPTION);
     }
 
     public InvalidMoveException(String message) {
