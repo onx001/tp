c6228d4b5295c83620b219a9fc6cf9baf45e4988
TriciaBK <e1032477@u.nus.edu>
2023-10-18 18:40:09 +0800

    Updated main class

--- src/main/java/chessmaster/ChessMaster.java
+++ src/main/java/chessmaster/ChessMaster.java
@@ -1,13 +1,15 @@
 package chessmaster;
 
+import chessmaster.exceptions.ParseCoordinateException;
 import chessmaster.game.ChessBoard;
 import chessmaster.ui.TextUI;
+
 
 /**
  * Main entry-point for ChessMaster application.
  */
 public class ChessMaster {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseCoordinateException {
 
         // String logo = "░█████╗░██╗░░██╗███████╗░██████╗░██████╗
         // ███╗░░░███╗░█████╗░░██████╗████████╗███████╗██████╗░"
@@ -34,13 +36,16 @@ public class ChessMaster {
         ChessBoard board = new ChessBoard();
         board.showChessBoard(ui);
 
-        // while (true) {
-        // board.showChessBoard(ui);
-        // String move = ui.getUserCommand();
-        // TODO: Check if move is valid
-        // TODO: Update chessboard (new position with chesspiece, old position empty)
-        // TODO: Update ChessPiece (position)
-        // TODO: Store updated board in text file
-        // }
+        while (true) {
+            board.showChessBoard(ui);
+            String input = ui.getUserCommand();
+
+
+            // TODO: Check if move is valid
+            // TODO: Update chessboard (new position with chesspiece, old position empty)
+            // TODO: Update ChessPiece (position)
+            // TODO: Store updated board in text file
+        }
     }
 }
+
