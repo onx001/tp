57557e1600d7cec0f7c99201f5668cff55c3ca3d
antrikshdhand <>
2023-10-19 22:52:31 +0800

    Add Javadoc comments

--- src/main/java/chessmaster/user/CPU.java
+++ src/main/java/chessmaster/user/CPU.java
@@ -15,32 +15,49 @@ public class CPU extends Player {
         super(colour);
     }
 
+    /**
+     * The main function behind the CPU's logic, this function randomly selects an active (non-captured) piece
+     * from the CPU's pieces which has possible valid moves.
+     * @param board The board to extract the random move from.
+     * @return A random move
+     */
     public Move getRandomMove(ChessBoard board) {
         // 1. Get a random piece that
         //    - isn't captured
         //    - has possible legal moves
         ChessPiece randomPiece = getRandomPiece();
-        while (randomPiece.getCaptured()
-                || randomPiece.getFlattenedCoordinates(board.getBoard()).length == 0) {
+
+        // Need a cap on the number of pieces it checks to prevent an infinite loop when no moves are possible
+        // on the CPUs side.
+        int MAX_LOOP_ITERATIONS = 16;
+        int iter = 0;
+        while (iter < MAX_LOOP_ITERATIONS
+                && (randomPiece.getCaptured()
+                || randomPiece.getFlattenedCoordinates(board.getBoard()).length == 0)) {
             randomPiece = getRandomPiece();
+            iter++;
         }
 
-        Move randomMove = getRandomMoveFromPiece(randomPiece, board);
-
-        return randomMove;
+        return getRandomMoveFromPiece(randomPiece, board);
     }
 
+    /**
+     * Returns a random integer from the CPUs Piece array.
+     * @return
+     */
     private ChessPiece getRandomPiece() {
-        printAllPieces();
-
         return this.pieces.get(rand.nextInt(pieces.size()));
     }
 
+    /**
+     * This function returns any random valid move that a given piece can make on a given board.
+     * @param piece The piece to extract a random move from.
+     * @param board The board the piece is currently on.
+     * @return A random move the given piece can make on the given board.
+     */
     private Move getRandomMoveFromPiece(ChessPiece piece, ChessBoard board) {
-
         Coordinate[] allPossibleMoves = piece.getFlattenedCoordinates(board.getBoard());
         Coordinate randomDestination = allPossibleMoves[rand.nextInt(allPossibleMoves.length)];
-
         return new Move(piece.getPosition(), randomDestination, piece);
     }
 
