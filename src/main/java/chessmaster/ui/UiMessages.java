405004cdcbd17a84b09651c91f74991c97b81429
TongZhengHong <zhtong315@gmail.com>
2023-10-23 01:10:07 +0800

    Fix CI checks

--- src/main/java/chessmaster/ui/UiMessages.java
+++ src/main/java/chessmaster/ui/UiMessages.java
@@ -11,7 +11,8 @@ public class UiMessages {
         " \\____/_/ /_/\\___/____/____/  /_/  /_/\\__,_/____/\\__/\\___/_/     ",
     };
 
-    public static final String EXIST_PREV_GAME_MESSAGE = "Looks like you have a previous chess game ongoing. Would you like to continue? [y/n]";
+    public static final String EXIST_PREV_GAME_MESSAGE = "Looks like you have a previous chess game ongoing. " +
+        "Would you like to continue? [y/n]";
     public static final String CHOOSE_PLAYER_COLOR_MESSAGE = "Choose your starting color to start new game! [b/w]";
 
     public static final String PROMPT_PROMOTE_MESSAGE = "You can promote the pawn at %s! \n" +
