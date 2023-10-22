dd2257fb6cb082dbaf2175e58c66298bc43a0d71
TriciaBK <e1032477@u.nus.edu>
2023-10-23 05:07:42 +0800

    Update Help Command

--- src/main/java/chessmaster/commands/HelpCommand.java
+++ src/main/java/chessmaster/commands/HelpCommand.java
@@ -5,7 +5,12 @@ public class HelpCommand extends Command {
     public static final String HELP_COMAMND_STRING = "help";
 
     private static final String[] HELP_STRINGS = { 
-        "Seems like you need some help, here are the following commands to play: "
+        "Seems like you need some help, here are the following commands to play: \n" +
+        "Move piece - Input coordinate of piece, followed by coordinate to move to \n" +
+        "   Format: [column][row] [column][row] \n" +
+        "   E.g. a2 a3 \n" +
+        "Abort game - Exit programme \n" +
+        "   Format: abort \n"
     };
 
     @Override
