5764c5b12d20c38431e2e7c60aacccc25c02747d
Tricia Boo Koh <e1032477@u.nus.edu>
2023-11-02 04:28:41 +0800

    Update ViewLegendCommand

--- src/main/java/chessmaster/commands/ViewLegendCommand.java
+++ src/main/java/chessmaster/commands/ViewLegendCommand.java
@@ -10,19 +10,20 @@ public class ViewLegendCommand extends Command {
 
     private static final String[] LEGEND_STRINGS = {
         "Black pieces:",
-        "\"r\" represents a black rook.",
-        "\"n\" represents a black knight.",
-        "\"b\" represents a black bishop.",
-        "\"q\" represents a black queen.",
-        "\"k\" represents a black king.",
-        "\"p\" represents a black pawn.",
+        "\"R\" represents a black rook.",
+        "\"N\" represents a black knight.",
+        "\"B\" represents a black bishop.",
+        "\"Q\" represents a black queen.",
+        "\"K\" represents a black king.",
+        "\"P\" represents a black pawn.",
+        "   ",
         "White pieces:",
-        "\"R\" represents a white rook.",
-        "\"N\" represents a white knight.",
-        "\"B\" represents a white bishop.",
-        "\"Q\" represents a white queen.",
-        "\"K\" represents a white king.",
-        "\"P\" represents a white pawn"
+        "\"r\" represents a white rook.",
+        "\"n\" represents a white knight.",
+        "\"b\" represents a white bishop.",
+        "\"q\" represents a white queen.",
+        "\"k\" represents a white king.",
+        "\"p\" represents a white pawn"
 
     };
 
